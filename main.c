ab4daf40cfe6eaff58d0dd011cf6560c1ba230fe
Pat <patriversidestuff@gmail.com>
2026-03-29 23:57:23 +0100

    ignore executable, cd and exit support added.

--- main.c
+++ main.c
@@ -3,15 +3,20 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv){
     char buff[256];
     char *tokenarr[64];
+    char cwd[100];
     int count = 0;
     int status;
     pid_t p;
     while(1){
         count = 0;
+        printf("~%s\n", getcwd(cwd, 100));
+        printf("> ");
+        fflush(stdout);
         fgets(buff, sizeof(buff), stdin);
         buff[strcspn(buff, "\n")] = '\0';
         char *tokens = strtok(buff, " ");
@@ -21,6 +26,16 @@ int main(int argc, char **argv){
             tokens = strtok(NULL, " ");
         }
         tokenarr[count] = NULL;
+        if(strcmp(tokenarr[0], "exit") == 0){
+            exit(0);
+        } else if(strcmp(tokenarr[0], "cd") == 0){
+            if(!tokenarr[1]){
+                printf("Directory not specified.\n");
+                continue;
+            }
+            chdir(tokenarr[1]);
+            continue;
+        }
         p = fork();
         if(p<0){
             fprintf(stderr, "fork fail");
